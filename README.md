024021fd4c861c75787c8ccf2983ff77e958be7a
benben-miao <benben.miao@outlook.com>
2021-08-03 13:20:23 +0800

    Readme comment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JavaRServe
 Java project call R packages and functions realtime and remote serve.
+
+### 1.Source Code Comment
+
+![Source Code Comment]("./resource/readme/JavaRServe.png")
+
+### 2.Dependencies in POM
+
+![Dependencies in POM]("./resource/readme/JavaRServe2.png")
--- src/main/java/com/benben/miao/rserve_test/App.java
+++ src/main/java/com/benben/miao/rserve_test/App.java
@@ -4,10 +4,31 @@ import org.rosuda.REngine.REXP;
 import org.rosuda.REngine.Rserve.RConnection;
 
 /*
- * 1.R install package and run server: install.packages("Rserve"), library(Rserve), Rserve(debug = FALSE, port=6311, args = NULL, quote=(length(args) > 1), wait=FALSE)
- * 2.Add REngine and RServe dependencies to pom file.
- * 3.Import packages and connect RServe.
+ 1.# R install package and run server
+ # 安装模块和配置: 
+ install.packages("Rserve")
+ library(Rserve)
+ # 通过函数和参数运行服务
+ Rserve(debug = FALSE, port=6311, args = NULL, quote=(length(args) > 1), wait=FALSE)
+
+ 2.Add REngine and RServe dependencies to pom file.
+	<dependency>
+	    <groupId>org.rosuda.REngine</groupId>
+	    <artifactId>REngine</artifactId>
+	    <version>2.1.0</version>
+	</dependency>
+	<dependency>
+	    <groupId>org.rosuda.REngine</groupId>
+	    <artifactId>Rserve</artifactId>
+	    <version>1.8.1</version>
+	</dependency>
+	
+ 3.Import packages and connect RServe.
+	RConnection rc = null;
+	rc = new RConnection();
+    REXP x = rc.eval("R.version.string");
  */
+
 public class App 
 {
     public static void main( String[] args )
@@ -16,7 +37,7 @@ public class App
     	RConnection rc = null;
     	try {
     		rc = new RConnection(); //建立与Rserve的连接
-    		REXP x = rc.eval("R.version.string");//执行R语句
+    		REXP x = rc.eval("R.version.string"); //执行R语句
     		System.out.println(x.asString());
     	} catch (Exception e) {
     		e.printStackTrace();
